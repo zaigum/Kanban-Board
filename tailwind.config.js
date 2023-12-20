bcec60949eb4f8b4471a914a529edb1a88f7df42
zaigum <143170220+zaigum@users.noreply.github.com>
2023-12-20 20:45:19 +0500

    Kanban Board  1st

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "mainBackgroundColor": '#0D1117',
+        "columnBackgroundColor": '#161C22',
+      },
+    },
+  },
+  plugins: [
+    // Add any Tailwind CSS plugins here, if needed
+  ],
+};
